47e665bed7a49b2fb0dda3e811a9a021bb349106
kbr1009 <reo109r@icloud.com>
2021-12-09 00:09:17 +0900

    crudを実装

--- MyVideoAPI/Controllers/Controller.cs
+++ MyVideoAPI/Controllers/Controller.cs
@@ -1,28 +1,107 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 using MyVideoAPI.Models;
+//using System;
+//using Microsoft.Extensions.Logging;
+
+
 
 namespace MyVideoAPI.Controllers
 {
-    [Route("api/v1/[controller]")]  
+  [Route("api/v1/[controller]")]  
     [ApiController]  
-    public class videosController : ControllerBase  
+      public class videosController : ControllerBase  
     {  
-        private MyVideoApiContext myDbContext;  
-  
-        public videosController(MyVideoApiContext context)  
-        {  
-            myDbContext = context;  
-        }  
-  
-        [HttpGet]  
+      private MyVideoApiContext myDbContext;  
+
+      public videosController(MyVideoApiContext context)  
+      {  
+        myDbContext = context;  
+      }  
+
+      //GET : api/v1/videos
+      [HttpGet]  
         public IList<VideoModel> Get()  
         {  
-            return (this.myDbContext.MyVideos.ToList());  
+          return (this.myDbContext.MyVideos.ToList());  
         }  
+
+      //GET : api/v1/videos/{id}
+      [HttpGet("{id}")]
+      public async Task<ActionResult<VideoModel>> GetTodoItem(long id)
+      {
+        var myvideo = await myDbContext.MyVideos.FindAsync(id);
+
+        if (myvideo == null)
+        {
+          return NotFound();
+        }
+
+        return myvideo;
+      }
+
+      //POST : api/v1/videos
+      [HttpPost]
+      public async Task<ActionResult<VideoModel>> PostVideoItem(VideoModel videoModel)
+      {
+        myDbContext.MyVideos.Add(videoModel);
+        await myDbContext.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(Get), new { id = videoModel.ID }, videoModel);
+      }
+
+      //PUT : api/v1/videos
+      [HttpPut("{id}")]
+      public async Task<IActionResult> PutTodoItem(long id, VideoModel myvideo)
+      {
+        if (id != myvideo.ID)
+        {
+          return BadRequest();
+        }
+
+        myDbContext.Entry(myvideo).State = EntityState.Modified;
+
+        try
+        {
+          await myDbContext.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+          if (!VideoModelExists(id))
+          {
+            return NotFound();
+          }
+          else
+          {
+            throw;
+          }
+        }
+
+        return NoContent();
+      }
+
+      // DELETE: api/v1/videos/{id}
+      [HttpDelete("{id}")]
+      public async Task<ActionResult<VideoModel>> DeleteTodoItem(long id)
+      {
+        var myvideo = await myDbContext.MyVideos.FindAsync(id);
+        if (myvideo == null)
+        {
+          return NotFound();
+        }
+
+        myDbContext.MyVideos.Remove(myvideo);
+        await myDbContext.SaveChangesAsync();
+
+        return myvideo;
+      }
+
+      private bool VideoModelExists(long id)
+      {
+        return myDbContext.MyVideos.Any(e => e.ID == id);
+      }
     }  
 }
